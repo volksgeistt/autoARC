9723826aceaf77cf45a8a764d8551ff741b01733
Ujjawal Singh <mastermindus07@gmail.com>
2026-03-06 12:29:46 +0530

    Add feature initialization in __init__.py
    Initialize feature imports and create FEATURE_MAP.

---
+++ features/__init__.py
@@ -0,0 +1,23 @@
+from . import (
+    autotyper,
+    automove,
+    mouse_simulation,
+    screen_capture,
+    timed_execution,
+    custom_key_sequence,
+    mouse_360,
+    auto_clicker,
+    webhook_settings,
+)
+
+FEATURE_MAP = {
+    1: autotyper,
+    2: automove,
+    3: mouse_simulation,
+    4: screen_capture,
+    5: timed_execution,
+    6: custom_key_sequence,
+    7: mouse_360,
+    8: auto_clicker,
+    9: webhook_settings,
+}
